9f2a05ee56fb253ad9a8922710cb49a0e3414c45
Rafał Selewońko <rafal@selewonko.com>
2026-04-01 09:37:04 +0200

    Add error handling with timeout to Nominatim requests, guard against missing entities and location attributes, switch to async platform setup, add PLATFORM_SCHEMA config validation, fix deprecated device_state_attributes, and fix template whitespace.

--- custom_components/osm_geocode/sensor.py
+++ custom_components/osm_geocode/sensor.py
@@ -4,17 +4,33 @@ import logging
 from datetime import timedelta
 
 import requests
-from homeassistant.const import ATTR_LATITUDE, ATTR_LONGITUDE
+import voluptuous as vol
+from homeassistant.components.sensor import PLATFORM_SCHEMA
+from homeassistant.const import ATTR_LATITUDE, ATTR_LONGITUDE, CONF_NAME
 from homeassistant.helpers import template as templater
 from homeassistant.helpers.entity import Entity
-
-# from homeassistant.helpers.location import has_location
 from homeassistant.util import Throttle
+import homeassistant.helpers.config_validation as cv
 
-__version__ = "0.1.0"
-logger = logging.getLogger(__name__)
+__version__ = "0.2.0"
+_LOGGER = logging.getLogger(__name__)
 
 NOMINATIM_URL = "https://nominatim.openstreetmap.org/reverse"
+
+CONF_SOURCE = "source"
+CONF_TEMPLATE = "template"
+CONF_ICON = "icon"
+
+DEFAULT_ICON = "mdi:map-marker"
+
+PLATFORM_SCHEMA = PLATFORM_SCHEMA.extend(
+    {
+        vol.Required(CONF_SOURCE): cv.entity_id,
+        vol.Required(CONF_NAME): cv.string,
+        vol.Optional(CONF_TEMPLATE): cv.string,
+        vol.Optional(CONF_ICON, default=DEFAULT_ICON): cv.string,
+    }
+)
 
 
 def get_address(latitude, longitude):
@@ -28,29 +44,36 @@ def get_address(latitude, longitude):
         ("format", "geojson"),
     )
 
-    response = requests.get(NOMINATIM_URL, headers=headers, params=params)
+    response = requests.get(
+        NOMINATIM_URL, headers=headers, params=params, timeout=10
+    )
+    response.raise_for_status()
 
-    address = response.json()["features"][0]["properties"].copy()
+    data = response.json()
+    features = data.get("features")
+    if not features:
+        raise ValueError("No features returned from Nominatim")
+
+    address = features[0]["properties"].copy()
     address.update(address["address"])
     del address["address"]
     return address
 
 
-DEFAULT_TEMPLATE = """
-{% if zone %}
-    {{ zone }}
-{% else %}
-    {% if name %}
-        {{ name }},
-    {% endif %}
-    {{ house_number }} {{ road }},
-    {{ city_district }}, {{ city }}
-{% endif %}
-"""
+DEFAULT_TEMPLATE = """\
+{% if zone %}\
+{{ zone }}\
+{% else %}\
+{% if name %}\
+{{ name }}, \
+{% endif %}\
+{{ house_number }} {{ road }}, \
+{{ city_district }}, {{ city }}\
+{% endif %}"""
 
 
-def setup_platform(hass, config, add_entities, discovery_info=None):
-    add_entities([OSMGeocodeSensor(hass, config)])
+async def async_setup_platform(hass, config, async_add_entities, discovery_info=None):
+    async_add_entities([OSMGeocodeSensor(hass, config)])
 
 
 class OSMGeocodeSensor(Entity):
@@ -61,24 +84,41 @@ class OSMGeocodeSensor(Entity):
         self._state = "Loading..."
 
     @Throttle(timedelta(seconds=60))
-    def update(self):
-        source = self.config.get("source")
+    async def async_update(self):
+        source = self.config.get(CONF_SOURCE)
         entity = self.hass.states.get(source)
 
-        # if has_location(entity):
+        if entity is None:
+            _LOGGER.warning("Entity %s not found", source)
+            return
+
         latitude = entity.attributes.get(ATTR_LATITUDE)
         longitude = entity.attributes.get(ATTR_LONGITUDE)
-        self.address = get_address(latitude, longitude)
+
+        if latitude is None or longitude is None:
+            _LOGGER.warning(
+                "Entity %s has no location attributes", source
+            )
+            return
+
+        try:
+            self.address = await self.hass.async_add_executor_job(
+                get_address, latitude, longitude
+            )
+        except (requests.RequestException, ValueError, KeyError) as err:
+            _LOGGER.error("Error fetching address: %s", err)
+            return
+
         self.address.update(
             {"zone": entity.state, "latitude": latitude, "longitude": longitude}
         )
 
-        template = self.config.get("template", DEFAULT_TEMPLATE)
+        template = self.config.get(CONF_TEMPLATE, DEFAULT_TEMPLATE)
         self._state = templater.Template(template, self.hass).render(self.address)
 
     @property
     def name(self):
-        return self.config.get("name")
+        return self.config.get(CONF_NAME)
 
     @property
     def state(self):
@@ -86,8 +126,8 @@ class OSMGeocodeSensor(Entity):
 
     @property
     def icon(self):
-        return self.config.get("icon", "mdi:map-marker")
+        return self.config.get(CONF_ICON, DEFAULT_ICON)
 
     @property
-    def device_state_attributes(self):
+    def extra_state_attributes(self):
         return self.address
